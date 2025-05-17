d37575b4e5c9d978fc4074aeeafd962f5d82d052
Joshua Layhue <joshua.layhue@gmail.com>
2025-05-17 07:57:16 -0400

    Adding amplify_outputs.json config file

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import './index.css';
 
 import { Amplify } from 'aws-amplify';
 // TEMPORARILY REMOVE AMPLIFY CONFIG TO UNBLOCK BUILD
-// import config from './aws-exports';
-// Amplify.configure(config);
+import config from '../amplify_outputs.json'; // adjust path if needed
+Amplify.configure(config);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
